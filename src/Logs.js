dad43e77039c30fc4463518fa2cc6ff93dc4a9b1
Maciej <value2k@gmail.com>
2018-07-25 09:23:16 +0200

    Logi zmiany nazwy użytkownika, edycji wiadomości, usunięcia wiadomości

---
+++ src/Logs.js
@@ -0,0 +1,109 @@
+const Discord = require("./Discord.js");
+const Config = require("./Config.js");
+
+Discord.on("messageDelete", function (message) {
+	let channel = Discord.channels.get(Config.getKeyValue("logs-channel-id"));
+	if (!channel) return console.log("Kanał od logów nie istnieje, nie można dodać wpisu!");
+
+	let data = new Date();
+	channel.send("", {
+		embed: {
+			description: `Wiadomość napisana przez ${message.author} została właśnie usunięta!`,
+			color: 16738814,
+			author: {
+				name: message.author.tag,
+				icon_url: message.author.avatarURL
+			},
+			fields: [
+				{
+					name: "Treść wiadomości",
+					value: message.content,
+					inline: false
+				},
+				{
+					name: "Kanał",
+					value: "" + message.channel,
+					inline: false
+				},
+				{
+					name: "Data",
+					value: data.toLocaleDateString() + " " + data.toLocaleTimeString(),
+					inline: false
+				}
+			]
+		}
+	});
+});
+
+Discord.on("messageUpdate", function (oldMessage, newMessage) {
+	let channel = Discord.channels.get(Config.getKeyValue("logs-channel-id"));
+	if (!channel) return console.log("Kanał od logów nie istnieje, nie można dodać wpisu!");
+
+	let data = new Date();
+	if (oldMessage.content && newMessage.content) channel.send("", {
+		embed: {
+			description: `Wiadomość napisana przez ${newMessage.author} została właśnie edytowana!`,
+			color: 16738814,
+			author: {
+				name: newMessage.author.tag,
+				icon_url: newMessage.author.avatarURL
+			},
+			fields: [
+				{
+					name: "Poprzednia treść",
+					value: oldMessage.content,
+					inline: false
+				},
+				{
+					name: "Nowa treść",
+					value: newMessage.content,
+					inline: false
+				},
+				{
+					name: "Kanał",
+					value: "" + newMessage.channel,
+					inline: false
+				},
+				{
+					name: "Data",
+					value: data.toLocaleDateString() + " " + data.toLocaleTimeString(),
+					inline: false
+				}
+			]
+		}
+	});
+});
+
+Discord.on("guildMemberUpdate", function (oldMember, newMember) {
+	let channel = Discord.channels.get(Config.getKeyValue("logs-channel-id"));
+	if (!channel) return console.log("Kanał od logów nie istnieje, nie można dodać wpisu!");
+
+	let data = new Date();
+	channel.send("", {
+		embed: {
+			description: `Użytkownik ${newMember} właśnie zmienił swój nickname!`,
+			color: 16738814,
+			author: {
+				name: newMember.tag,
+				icon_url: newMember.avatarURL
+			},
+			fields: [
+				{
+					name: "Nowa nazwa użytkownika",
+					value: (newMember.nickname ? newMember.nickname : newMember.user.username),
+					inline: false
+				},
+				{
+					name: "Poprzednia nazwa użytkownika",
+					value: (oldMember.nickname ? oldMember.nickname : oldMember.user.username),
+					inline: false
+				},
+				{
+					name: "Data",
+					value: data.toLocaleDateString() + " " + data.toLocaleTimeString(),
+					inline: false
+				}
+			]
+		}
+	});
+});
