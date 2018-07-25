bd29aed8878bd243bf69b5e4b5612d6ae7fd58e6
Maciej <value2k@gmail.com>
2018-07-25 09:23:25 +0200

    Opisy komend

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
 const Config = require("./src/Config.js");
 const Commands = require("./src/Commands.js");
 const Discord = require("./src/Discord.js");
+const Logs = require("./src/Logs.js");
 
 
 // Komendy podstawowe
-Commands.registerCommand("about", require("./src/Commands/Misc.js").About);
-Commands.registerCommand("help", require("./src/Commands/Misc.js").Help);
-Commands.registerCommand("avatar", require("./src/Commands/Misc.js").Avatar);
+Commands.registerCommand("about", "Wyświetla informacje na temat bota", require("./src/Commands/Misc.js").About);
+Commands.registerCommand("help", "Wysyła listę komend w wiadomości bezpośredniej", require("./src/Commands/Misc.js").Help);
+Commands.registerCommand("avatar", "Wyświetla awatar oznaczonego użytkownika", require("./src/Commands/Misc.js").Avatar);
 
 // Komendy administracyjne
 Commands.registerCommand("kick", require("./src/Commands/Administration.js").kickMember);
 Commands.registerCommand("warn", require("./src/Commands/Administration.js").warnMember);
 
 // Gry i zabawy
-Commands.registerCommand("moneta", require("./src/Commands/Games.js").FlipCoin);
-Commands.registerCommand("fortnite", require("./src/Commands/Games.js").Fortnite);
-Commands.registerCommand("csgo", require("./src/Commands/Games.js").CounterStrike);
-Commands.registerCommand("rocketleague", require("./src/Commands/Games.js").RocketLeague);
+Commands.registerCommand("moneta", "Wykonuje rzut monetą", require("./src/Commands/Games.js").FlipCoin);
+Commands.registerCommand("fortnite", "Wyświetla statystyki Fortnite Battle Royale", require("./src/Commands/Games.js").Fortnite);
+Commands.registerCommand("csgo", "Wyświetla statystyki Counter-Strike: Global Offensive", require("./src/Commands/Games.js").CounterStrike);
+Commands.registerCommand("rocketleague", "Wyświetla statystyki Rocket League", require("./src/Commands/Games.js").RocketLeague);
 
 
 // Eventy botowe
